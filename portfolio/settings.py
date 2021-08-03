0331da0df1898540e1141eb714f0a9f962734b5c
azimxxm <azimxxm@gmail.com>
2021-08-03 11:05:05 +0500

    deplos

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -113,7 +113,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
