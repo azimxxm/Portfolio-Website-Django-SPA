c90316cfd6974fe6a74cf0cb2528cfb661ed3785
azimxxm <azimxxm@gmail.com>
2021-08-19 12:14:53 +0500

    full

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import *
+from .forms import *
 
 def index(request):
+    about = About.objects.all()
+    portfolio = Portfolio.objects.all()
+    form = ContactForm
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {
+        'about':about,
+        'portfolio':portfolio,
+    }
+    return render(request, 'jobs/index.html', context)
+
+def message(request):
+
     return render(request, 'jobs/index.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jobs.urls'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
